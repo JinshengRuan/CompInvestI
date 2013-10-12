eee262b6d5d8a13b61706309d72a65314afb6bb1
Jinsheng Ruan <jinsheng.ruan@gmail.com>
2013-10-12 10:42:46 +0800

    add quiz6

---
+++ quiz6.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import math
+import copy
+import QSTK.qstkutil.qsdateutil as du
+import datetime as dt
+import QSTK.qstkutil.DataAccess as da
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkstudy.EventProfiler as ep
+
+if __name__ == '__main__':
+    dt_start = dt.datetime(2010, 4, 1)
+    dt_end = dt.datetime(2010, 5, 31)
+    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
+
+    dataobj = da.DataAccess('Yahoo')
+    ls_symbols = ["AAPL", "MSFT"]
+    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+    ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+    d_data = dict(zip(ls_keys, ldf_data))
+
+    for s_key in ls_keys:
+        d_data[s_key] = d_data[s_key].fillna(method='ffill')
+        d_data[s_key] = d_data[s_key].fillna(method='bfill')
+        d_data[s_key] = d_data[s_key].fillna(1.0)
+
+    df_close = d_data['close']
+    df_mean = pd.rolling_mean(df_close, 20)
+    df_std = pd.rolling_std(df_close, 20)
+
+    df_bands = (df_close - df_mean) / df_std
+
+    print df_bands
